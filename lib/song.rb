c424e92ede18266e111a6b8786f4c87490020778
Pe-ngo <jamespengo95@gmail.com>
2022-09-12 08:44:16 +0300

    Completed Assignment

---
+++ lib/song.rb
@@ -0,0 +1,43 @@
+class Song
+
+    attr_reader :name, :artist, :genre
+
+    @@count = 0
+    @@artists = []
+    @@genres = []
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@artists << self.artist
+        @@genres << @genre
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.genre_count
+        @@genres.tally
+    end
+
+    def self.artist_count
+        @@artists.tally
+    end
+
+end
+
+
+Song.new("Lucifer", "Jay-Z", "rap")
+Song.new("99 Problems", "Jay-Z", "rap")
+Song.new("hit me baby one more time", "Brittany Spears", "pop")
